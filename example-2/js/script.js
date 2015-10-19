e3a649149366d707f9aec57246c4117936b0fa34
canerk <cannerk89@gmail.com>
2015-10-19 14:10:18 -0400

    simple circle examples
    blah de blah

---
+++ example-2/js/script.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("div")[0].onclick = function(){
+    divWider();
+}
+
+function divWider(){
+    //alert("yes");
+    document.getElementsByTagName("div")[0]
+.className="wider";
+}
